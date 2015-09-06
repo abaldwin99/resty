876b267d9f8025e3a3350a9c979f6332f5f14a17
Alex Baldwin <abaldwintech@gmail.com>
2015-09-06 10:10:42 -0400

    ngResource basic setup

--- public/resty.js
+++ public/resty.js
@@ -1,7 +1,20 @@
 var app = angular.module('restyApp', ['ngRoute', 'ngResource']);
 
 
-app.controller('restyController', function () {
+app.factory('Post', function ($resource) {
+    return $resource('/posts/:id');
+});
+
+
+app.controller('restyController', function ($scope, Post) {
     var resty = this;
     resty.go = 5;
+
+    var posts = Post.query(function () {
+        console.log(posts);
+        resty.go = posts;
+    });
+
+
+
 });
