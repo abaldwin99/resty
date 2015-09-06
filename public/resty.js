fa0a2019600956af330e278c8424ae1941f52539
Alex Baldwin <abaldwintech@gmail.com>
2015-09-06 11:02:55 -0400

    delete and update methods

--- public/resty.js
+++ public/resty.js
@@ -2,7 +2,13 @@ var app = angular.module('restyApp', ['ngRoute', 'ngResource']);
 
 
 app.factory('Post', function ($resource) {
-    return $resource('/posts/:id');
+    return $resource('/posts/:id', {
+        id: '@id'
+    }, {
+        update: {
+            method: 'PUT' // this method issues a PUT request
+        }
+    });
 });
 
 
@@ -15,18 +21,34 @@ app.controller('restyController', function ($scope, Post) {
     });
 
     var onePost = Post.get({
-        id: 1
+        id: 2
     }, function () {
         console.log(onePost.title)
     });
 
-    var newPost = new Post();
-    newPost.data = {
+    newPost = {
         "title": "asdf",
         "author": "asdf",
     }
     Post.save(newPost, function () {
         console.log('saved')
+    });
+
+    var updatePost = Post.get({
+        id: 4
+    }, function () {
+        updatePost.title = 'something else';
+        updatePost.$update(function () {
+            console.log('updated id 1')
+        });
+    });
+
+    var deletePost = Post.get({
+        id: 8
+    }, function () {
+        deletePost.$delete(function () {
+            console.log('id 3 is deleted')
+        });
     });
 
 
